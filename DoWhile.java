f2b292b55d62fe43a43274e445716a0fbfd89835
Bhargavi Nagaraju <bhargavi.nagaraju10@gmail.com>
2025-06-26 10:19:52 +0530

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,16 @@
+// print 1 to 10 numbers
+public class DoWhile{
+public static void main (String[]args){
+// Step 1: Initialize the variable i with 1
+int i = 1;
+// Step 2: Start the do-while loop
+do{
+// Step 3: Print the current value of i
+System.out.print(i + " ");
+// Step 4: Increment i by 1
+i++;
+}while (i<=10);
+// Step 5: Continue the loop as long as i is less than or equal to 10
+
+}
+}
